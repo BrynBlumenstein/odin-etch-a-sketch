6a5131ffaa3377cb02dc0dc0b61319733baca3c3
BrynBlumenstein <90479522+BrynBlumenstein@users.noreply.github.com>
2024-01-23 19:21:38 -0500

    Add functional Etch-a-Sketch and create new button

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 const grid = document.querySelector('#grid');
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.classList.add("square");
-    grid.appendChild(square);
+const newGridButton = document.querySelector('#create-new');
+
+generateGrid(100);
+
+newGridButton.addEventListener('click', () => {
+    let squaresPerRow = getSquaresPerRow();
+    generateGrid(squaresPerRow);
+});
+
+function getSquaresPerRow() {
+    let squaresPerRow;
+
+    do {
+        squaresPerRow = prompt('Enter the number of squares per row (1-100)');
+        squaresPerRow = parseInt(squaresPerRow);
+    } while (isNaN(squaresPerRow) || squaresPerRow < 1 || squaresPerRow > 100);
+
+    return squaresPerRow;
+}
+
+function generateGrid(squaresPerRow) {
+    grid.innerHTML = '';
+
+    const squares = squaresPerRow**2
+
+    for (let i = 0; i < squares; i++) {
+        let square = document.createElement('div');
+
+        square.classList.add('square');
+        square.style.width = `calc(100% / ${squaresPerRow})`;
+        square.style.height = `calc(100% / ${squaresPerRow})`;
+        square.addEventListener('mouseover', () => square.style.backgroundColor = 'black');
+
+        grid.appendChild(square);
+    }
 }
